d09d8151c0628e72a58ac84ee6a9e64fc9a6e378
Rafa P <kadetito@gmail.com>
2019-07-14 19:48:21 +0200

    re
    ref

--- administrador/includes/smartyvariablesinicio.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set('Europe/Madrid');
-$smarty = new Smarty;
-$smarty->force_compile = true;//fuerza eliminar cache(comentar esta linea al acabar)
-$smarty->debugging = true;
-$smarty->caching = true;
-$smarty->cache_lifetime = 120;
--- administrador/templates/test.tpl
+++
@@ -1,6 +0,0 @@
-<form action="" method="POST">
-<input type="submit" name="accion" value="Accion" />
-</form>
-<form action="" method="POST">
-<input type="submit" name="fulminar" value="Fulminar" />
-</form>
